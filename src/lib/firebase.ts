8149408eeb7cc9153a000fd4e8420f72d3fb5123
Rajat Krishnan <rajatkrishnan2002@gmail.com>
2026-05-30 18:33:16 +0530

    Inline firebaseConfig and update Firestore initialization
    Replaced firebaseConfig import with inline configuration and updated Firestore initialization.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "../../firebase-applet-config.json";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA31A_jBgpHWgX9_1d6rIO5JtD_BbyiWOo",
+  authDomain: "chat-buddy-90ecb.firebaseapp.com",
+  projectId: "chat-buddy-90ecb",
+  storageBucket: "chat-buddy-90ecb.firebasestorage.app",
+  messagingSenderId: "61203474928",
+  appId: "1:61203474928:web:279db7cf1f2086d86b7603",
+  measurementId: "G-8E30N6GSFV"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
